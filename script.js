ccd1ec127f68d635c54f1599f44b823d014ca570
Isaac-White <isaacwhite906@gmail.com>
2025-07-01 14:28:22 -0400

    Unit 3 project

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Declare variables below to save the different divs of your story.
-
-
+let storyOpening = document.querySelector('.story-opening');
+let optionOneButton = document.querySelector('.option-one');
+let optionTwoButton = document.querySelector('.option-two');
+let optionOneScreen = document.querySelector('.option-one-screen');
+let optionTwoScreen = document.querySelector('.option-two-screen');
+let optionOneEnd = document.querySelector('.option-one-end');
+let optionTwoEnd = document.querySelector('.option-two-end');
 
 
 
@@ -10,8 +15,37 @@
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
+storyOpening.addEventListener('click', function() {
+    storyOpening.style.display = 'block';
+    optionOneButton.style.display = "none";   
+    optionTwoButton.style.display = "none";
+    optionOneScreen.style.display = 'none';
+    optionTwoScreen.style.display = 'none';
+    optionOneEnd.style.display = 'none';
+    optionTwoEnd.style.display = 'none';
+
+});
+
+optionOneButton.addEventListener('click', function() {
+    optionOneButton.style.display = 'none';
+    optionTwoButton.style.display = 'none';
+    optionOneScreen.style.display = 'block';
+    optionTwoEnd.style.display = 'none';
+    optionOneEnd.style.display = 'block';
+    optionTwoScreen.style.display = 'none';
+    storyOpening.style.display = 'none';
+});
 
 
+optionTwoButton.addEventListener('click', function() {
+    optionOneButton.style.display = 'none';
+    optionTwoButton.style.display = 'none';
+    optionTwoScreen.style.display = 'block';
+    optionOneEnd.style.display = 'none';
+    optionTwoEnd.style.display = 'block';
+    optionOneScreen.style.display = 'none';
+    storyOpening.style.display = 'none';    
+}); 
 // INSERT_VARIABLE.addEventListener('click', function(){
 
 // });
